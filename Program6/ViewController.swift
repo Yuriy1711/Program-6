92fbc3ee5c152d86e318145592b9e3142aaae350
yuriy1711 <y.kryskiv@setonhill.edu>
2022-04-20 16:58:25 -0400

    Initial Commit

---
+++ Program6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Program6
+//
+//  Created by Yuriy Kryskiv on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
